cae8db57ceb7c0b826f4855774fa211075ff911d
Mahedi Hasan <bijoycse2014@gmail.com>
2022-08-22 23:48:51 +0600

    first commit with finished templating

---
+++ resources/views/backend/pages/product/addproduct.blade.php
@@ -0,0 +1,28 @@
+@extends('backend.mastering.master')
+
+
+
+@section('content')
+
+
+	<form action="">
+		<div class="form-group">
+			<label for="">Name</label>
+			<input type="text" class="form-control">
+		</div>
+		<div class="form-group">
+			<label for="">Email</label>
+			<input type="text" class="form-control">
+		</div>
+		<div class="form-group">
+			<label for="">Phone</label>
+			<input type="text" class="form-control">
+		</div>
+		<div class="form-group">
+			<label for="">Status</label>
+			<input type="text" class="form-control">
+		</div>
+	</form>
+
+
+@endsection
